1198b681ea2e906311d64c5fcce31aed968b61cb
Andrei Punko <andd3dfx@gmail.com>
2024-10-15 20:31:08 +0300

    Extract methods in ParabolicEquationTest. Add link to it in README

--- src/test/java/by/andd3dfx/math/pde/equation/ParabolicEquationTest.java
+++ src/test/java/by/andd3dfx/math/pde/equation/ParabolicEquationTest.java
@@ -16,24 +16,31 @@ class ParabolicEquationTest {
     void solve() {
         var h = THICKNESS / 1000.0;
         var tau = TIME / 1000.0;
-
-        var diffusionEquation = new ParabolicEquation(0, THICKNESS, TIME, new BorderConditionType1() {
-            @Override
-            public double gU(double t) {
-                return C_LEFT;
-            }
-        }, new BorderConditionType1()) {
-            @Override
-            protected double gK(double x, double t, double U) {
-                return DIFFUSION_COEFFICIENT;
-            }
-        };
+        var diffusionEquation = buildParabolicEquation();
 
         diffusionEquation.solve(h, tau);
 
         diffusionEquation.sUt("./build/res-numeric.txt", TIME);
         FileUtil.saveFunc(new Interval(0, THICKNESS, h),
                 (x) -> analyticSolution(x, TIME), "./build/res-analytic.txt");
+        // TODO compare numeric & analytic solutions
+    }
+
+    private ParabolicEquation buildParabolicEquation() {
+        var leftBorderCondition = new BorderConditionType1() {
+            @Override
+            public double gU(double t) {
+                return C_LEFT;
+            }
+        };
+        var rightBorderCondition = new BorderConditionType1();
+
+        return new ParabolicEquation(0, THICKNESS, TIME, leftBorderCondition, rightBorderCondition) {
+            @Override
+            protected double gK(double x, double t, double U) {
+                return DIFFUSION_COEFFICIENT;
+            }
+        };
     }
 
     private double analyticSolution(double x, double t) {
