77da9edaef0220e2e8651e1cac3e7c0380e47615
Andrei Punko <andd3dfx@gmail.com>
2024-10-22 23:30:55 +0300

    Add analytic solution for Parabolic PDE. Compare it with numeric solution. Fix #1

--- src/test/java/by/andd3dfx/math/pde/equation/ParabolicEquationTest.java
+++ src/test/java/by/andd3dfx/math/pde/equation/ParabolicEquationTest.java
@@ -4,30 +4,38 @@ import by.andd3dfx.math.pde.border.BorderConditionType1;
 import by.andd3dfx.util.FileUtil;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Math.PI;
+import static java.lang.Math.exp;
+import static java.lang.Math.sin;
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * <pre>
  * Solution of diffusion problem:
- * - plate with thickness d=1 mm
- * - constant concentration C=1.0 on left border and C=0.0 on right border
- * - constant diffusion coefficient D
+ * - plate with thickness L=1 mm
+ * - constant concentration C=0 on the left & right borders
+ * - initial concentration with triangle profile: most of mass concentrated in the center (see method getU0(x))
+ * - constant diffusion coefficient D=1e-9
  * </pre>
  */
 class ParabolicEquationTest {
 
-    private final double C_LEFT = 1.0;
-    private final double THICKNESS = 1e-3;  // 1mm
+    private final double C0 = 100.0;
+    private final double L = 1e-3;          // 1mm
     private final double TIME = 1;          // 1sec
-    private final double DIFFUSION_COEFFICIENT = 1e-4;
+    private final double D = 1e-9;
 
-    // We allow difference between numeric & analytic solution no more than 5% of max concentration value
-    private final double EPSILON = C_LEFT / 20.;
+    private final double h = L / 100.0;
+    private final double tau = TIME / 100.0;
+
+    // We allow difference between numeric & analytic solution no more than 1% of max concentration value
+    private final double EPSILON = C0 / 100.;
 
     @Test
     void solve() {
-        var h = THICKNESS / 1000.0;
-        var tau = TIME / 1000.0;
         var diffusionEquation = buildParabolicEquation();
 
+        // Solve equation
         diffusionEquation.solve(h, tau);
 
         // Save numeric solution to file
@@ -35,38 +43,85 @@ class ParabolicEquationTest {
         FileUtil.save(numericU, "./build/res-numeric.txt", true);
 
         // Save analytic solution to file
-        FileUtil.saveFunc(diffusionEquation.area.x(),
-                (x) -> analyticSolution(x, TIME), "./build/res-analytic.txt");
+        FileUtil.saveFunc(diffusionEquation.area.x(), (x) -> analyticSolution(x, TIME), "./build/res-analytic.txt");
 
         // Compare numeric & analytic solutions
         for (var i = 0; i < numericU.getN(); i++) {
+            var x = numericU.x(i);
             var numericY = numericU.y(i);
-            var analyticY = analyticSolution(numericU.x(i), TIME);
-            // TODO uncomment assert below after addition of analytic solution
-            // assertThat(Math.abs(numericY - analyticY)).isLessThanOrEqualTo(EPSILON);
+            var analyticY = analyticSolution(x, TIME);
+            assertThat(Math.abs(numericY - analyticY)).isLessThanOrEqualTo(EPSILON);
         }
     }
 
     private ParabolicEquation buildParabolicEquation() {
-        var leftBorderCondition = new BorderConditionType1() {
-            @Override
-            public double gU(double t) {
-                return C_LEFT;
-            }
-        };
+        var leftBorderCondition = new BorderConditionType1();
         var rightBorderCondition = new BorderConditionType1();
 
-        return new ParabolicEquation(0, THICKNESS, TIME, leftBorderCondition, rightBorderCondition) {
+        return new ParabolicEquation(0, L, TIME, leftBorderCondition, rightBorderCondition) {
             @Override
             protected double gK(double x, double t, double U) {
-                return DIFFUSION_COEFFICIENT;
+                return D;
+            }
+
+            @Override
+            protected double gU0(double x) {
+                return getU0(x);
             }
         };
     }
 
+    /**
+     * Initial concentration profile
+     *
+     * @param x space coordinate
+     * @return concentration C_0(x)
+     */
+    private double getU0(double x) {
+        x /= L;
+        if (0.4 <= x && x <= 0.5) {
+            return C0 * (10 * x - 4);
+        }
+        if (0.5 <= x && x <= 0.6) {
+            return C0 * (-10 * x + 6);
+        }
+        return 0;
+    }
+
+    /**
+     * <pre>
+     * Analytic solution:
+     * U(x,t) = Sum(b_n*u_n(x,t)) = Sum(b_n*sin(n*PI*x/L))*exp(-(n*PI/L)^2 * D*t)
+     *
+     * According to <a href="https://math.libretexts.org/Bookshelves/Differential_Equations/Differential_Equations_(Chasnov)/09%3A_Partial_Differential_Equations/9.05%3A_Solution_of_the_Diffusion_Equation">article</a>
+     * </pre>
+     *
+     * @param x space coordinate
+     * @param t time
+     * @return concentration C(x,t)
+     */
     private double analyticSolution(double x, double t) {
-        // TODO add analytic solution
-        var A = 1.5e+3;
-        return C_LEFT * Math.exp(-A * x);
+        var result = 0d;
+        for (int n = 0; n < 100; n++) {
+            result += b(n) * sin(n * PI * x / L) * exp(-n * n * PI * PI * D * t / (L * L));
+        }
+        return result;
+    }
+
+    /**
+     * Coefficient b_n of a Fourier sine series
+     *
+     * @param n number of coefficient
+     * @return b_n value
+     */
+    private double b(int n) {
+        var integral = 0d;
+        var N = 100d;
+        var dx = L / N;
+        for (int i = 0; i < N; i++) {
+            var x = dx * i;
+            integral += getU0(x) * sin(n * PI * x / L) * dx;
+        }
+        return 2. / L * integral;
     }
 }
