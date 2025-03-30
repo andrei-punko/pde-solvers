0a6dc5e8cf9b43d9d09a3c090f3acfe7b3600a7e
andrei-punko <andd3dfx@gmail.com>
2025-03-30 18:08:21 +0000

    Deploying to javadoc from @ andrei-punko/pde-solvers@b0a5910786c7511eb12e7a39033f64b297a43aee 🚀

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"by.andd3dfx.math.matrix"},{"l":"by.andd3dfx.math.pde.border"},{"l":"by.andd3dfx.math.pde.equation"},{"l":"by.andd3dfx.math.pde.solver"},{"l":"by.andd3dfx.math.space"},{"l":"by.andd3dfx.util"}];updateSearchResults();
